976d1d61bdf82c24e1cefa34d8805a6563165d4c
andreyszcr <60619582+andreyszcr@users.noreply.github.com>
2020-03-19 12:25:07 -0600

    cambios

---
+++ CapaDatos/Datos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+namespace CapaDatos
+{
+    public class Datos
+    {
+        public static string conexionBD(string servidor, string usuario, string contraseña, string basedatos)
+        {
+            var cadena = new SqlConnectionStringBuilder();
+            cadena.DataSource = servidor;
+            cadena.IntegratedSecurity = false;
+            cadena.UserID = usuario;
+            cadena.Password = contraseña;
+            cadena.InitialCatalog = basedatos;
+
+            var conexionBD = cadena.ToString();
+
+
+            return conexionBD;
+
+        }
+        //*******************************************************************************************************
+        public void Ingresar()
+        {
+
+        }
+        //*********************************************************************************************************
+        public void Registrarse()
+        {
+
+        }
+        //**
+
+    }
+}
